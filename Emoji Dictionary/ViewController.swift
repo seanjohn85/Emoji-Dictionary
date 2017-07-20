3e00ab4a41602084b218531313755b3d31f6650d
John <johnkenny1985@gamil.com>
2017-07-20 22:38:43 +0100

    created a tableview with 5 sample cells

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2017 JOHN KENNY. All rights reserved.
 //
 
+// creates 5 cell with John
+
 import UIKit
 
-class ViewController: UIViewController {
+//needed for the tableview
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    //tableview element from the view controller
+    @IBOutlet var table: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        table.dataSource = self
+        table.delegate = self
     }
 
+    //create 5 rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    //create 5 cells with the string "John"
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "John"
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
